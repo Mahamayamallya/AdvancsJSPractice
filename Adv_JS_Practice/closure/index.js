661b234972c66071a559c809cefc5963aaa517e4
Mahamaya Mallya <46756551+Mahamayamallya@users.noreply.github.com>
2024-06-10 10:01:52 +0530

    Update index.js

--- Adv_JS_Practice/closure/index.js
+++ Adv_JS_Practice/closure/index.js
@@ -1,26 +1,67 @@
-let x = 1;
+/* let x = 1;
 
-function a() {
-  let myValue = 2;
-  console.log(myValue);
+const parentFunction = () => {
+  let y = 2;
+  console.log("log1");
   console.log(x);
+  console.log(y);
 
-  function a2() {
-    console.log(myValue);
+  const childFunction = () => {
+    console.log("child function");
     console.log(x);
-  }
-  return a2;
-}
+    console.log((x += 5));
+    console.log((y += 1));
+    console.log("end");
+  };
 
-const result = a();
+  return childFunction();
+};
+
+const result = parentFunction;
 console.log(result);
-//result();
+// console.log(result);
 
+result();
+result();
+result();
+
+console.log(x); 
+-------------------------------------------------------
+
+*/
+
+/*
 //IIFE
 const privateCounter = (() => {
   let count = 0;
-  console.log(`initial value: ${count}`);
+  console.log(`initial value :' ${count}`);
+  return () => {
+    count += 1;
+    console.log(count);
+  };
 })();
 privateCounter();
 privateCounter();
 privateCounter();
+//second time onwards, only return value is displayed. 
+
+------------------------------------------------------------
+*/
+const credits = ((num) => {
+  let credits = num;
+
+  console.log(`initial value : ${credits}`);
+  return () => {
+    credits -= 1;
+    if (credits > 0) {
+      console.log(`playing game : ${credits}`);
+    } else {
+      console.log("not enough credits");
+    }
+  };
+})(3);
+
+credits();
+credits();
+credits();
+//when the function is not called, initial value is displayed as it is IIFE. When the function is called, only return is displayed
